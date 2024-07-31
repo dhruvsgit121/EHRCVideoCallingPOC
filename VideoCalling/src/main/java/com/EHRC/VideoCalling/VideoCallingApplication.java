46e6bd316e825e311390942bd708fc320e05c0fa
dhruvsgit <dhruv.sgit@gmail.com>
2024-07-31 10:39:15 +0530

    Initial files commit.

---
+++ VideoCalling/src/main/java/com/EHRC/VideoCalling/VideoCallingApplication.java
@@ -0,0 +1,13 @@
+package com.EHRC.VideoCalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCallingApplication.class, args);
+	}
+
+}
---
+++ VideoCalling/src/test/java/com/EHRC/VideoCalling/VideoCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EHRC.VideoCalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
